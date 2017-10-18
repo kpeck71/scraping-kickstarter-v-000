38545d633ece58006947ecd286b852b8650a7413
Kristen Peck <kpeck71@gmail.com>
2017-10-18 01:29:42 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,3 +10,7 @@ end
 
 create_project_hash
 #projects: kickstarter.css("li.project.grid_4").first
+#title: project.css("h2.bbcard_name strong a").text
+#image link: project.css("div.project-thumbnail a img").attribute("src").value
+#description: project.css("p.bbcard_blurb").text
+#location: project.css("ul.project-meta li span.location-name").text
